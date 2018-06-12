f76af2424f169a1a5fdecbdd44344b37b6b6d455
Bjarki Þór Elvarsson <bthe@users.noreply.github.com>
2018-06-12 15:51:58 +0000

    Update Gruntfile.js

--- libraries/frameworks/revealjs/Gruntfile.js
+++ libraries/frameworks/revealjs/Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 
 	// Project configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON(''),
 		meta: {
 			banner:
 				'/*!\n' +
